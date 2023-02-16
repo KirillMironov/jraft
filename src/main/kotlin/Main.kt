b35a04aecbe7ac2a31a7663db69669ded8acc4c6
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2023-02-16 19:05:45 +0300

    MainKt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,15 @@
 import java.nio.file.Files.*
 
-object Main {
+fun main(args: Array<String>) {
+    val groupId = "jraft"
+    val serverId = args[0]
+    val peers = listOf("localhost:8080", "localhost:8081", "localhost:8082")
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val groupId = "jraft"
-        val serverId = args[0]
-        val peers = listOf("localhost:8080", "localhost:8081", "localhost:8082")
+    val logsPath = createTempDirectory(serverId.split(":")[1]).toString()
 
-        val logsPath = createTempDirectory(serverId.split(":")[1]).toString()
+    val stateMachine = StateMachine()
 
-        val stateMachine = StateMachine()
+    val node = Node(groupId, serverId, peers, logsPath, stateMachine)
 
-        val node = Node(groupId, serverId, peers, logsPath, stateMachine)
-
-        node.start()
-    }
-
+    node.start()
 }
