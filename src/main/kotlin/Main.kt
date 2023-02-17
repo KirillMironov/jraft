90f06687784945584ad82cf2f20bdbbbb3b394f7
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2023-02-17 18:28:38 +0300

    Node

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 import java.nio.file.Files.createTempDirectory
 
+const val groupId = "jraft-example"
+
 fun main(args: Array<String>) {
-    val groupId = "jraft"
     val serverId = args[0]
+
     val peers = listOf("localhost:8080", "localhost:8081", "localhost:8082")
 
     val logsPath = createTempDirectory(serverId.split(":")[1]).toString()
--- src/main/kotlin/Node.kt
+++ src/main/kotlin/Node.kt
@@ -5,16 +5,16 @@ import com.alipay.sofa.jraft.option.NodeOptions
 import org.slf4j.LoggerFactory
 
 class Node(
-    private val groupId: String,
-    private val serverId: String,
-    private val peers: List<String>,
+    groupId: String,
+    serverId: String,
+    peers: List<String>,
     logsDirectory: String,
     stateMachine: StateMachine,
+    electionTimeoutMs: Int = 5000,
 ) {
-    private val raftGroupService: RaftGroupService
-    private val node: com.alipay.sofa.jraft.Node
-
     private val logger = LoggerFactory.getLogger(javaClass)
+
+    private val raftGroupService: RaftGroupService
 
     init {
         val options = NodeOptions()
@@ -24,21 +24,16 @@ class Node(
         options.logUri = logsDirectory
         options.raftMetaUri = logsDirectory
         options.snapshotUri = logsDirectory
-        options.electionTimeoutMs = 5000
-        options.snapshotIntervalSecs = 30
+        options.electionTimeoutMs = electionTimeoutMs
 
         val peerId = JRaftUtils.getPeerId(serverId)
 
         raftGroupService = RaftGroupService(groupId, peerId, options)
-
-        node = raftGroupService.start()
     }
 
     fun start() {
-        logger.info("Starting JRaft node with groupId: $groupId, serverId: $serverId, and peers: $peers")
-    }
+        val node = raftGroupService.start()
 
-    fun shutdown() {
-        raftGroupService.shutdown()
+        logger.info("Starting node with groupId: ${node.groupId}, nodeId: ${node.nodeId}, and peers: ${node.listPeers()}")
     }
 }
