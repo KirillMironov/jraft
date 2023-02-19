d48469de4cab83f584f774a790982bb2e1f02461
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2023-02-19 12:25:30 +0300

    StateMachine

--- src/main/kotlin/StateMachine.kt
+++ src/main/kotlin/StateMachine.kt
@@ -6,29 +6,33 @@ import com.alipay.sofa.jraft.error.RaftException
 import org.slf4j.LoggerFactory
 
 class StateMachine : StateMachineAdapter() {
+
     private val logger = LoggerFactory.getLogger(javaClass)
 
     override fun onApply(p0: Iterator?) {
-        logger.info("onApply: {}", p0)
-    }
-
-    override fun onError(e: RaftException?) {
-        logger.error("onError: {}", e.toString())
+        p0?.forEach { task ->
+            logger.info("Task applied: {}", task)
+        }
     }
 
     override fun onLeaderStart(p0: Long) {
-        logger.info("onLeaderStart: {}", p0)
+        logger.info("Node is now a leader: {}", p0)
     }
 
     override fun onLeaderStop(p0: Status?) {
-        logger.info("onLeaderStop: {}", p0)
+        logger.info("Node is no longer a leader: {}", p0)
     }
 
     override fun onStartFollowing(p0: LeaderChangeContext?) {
-        logger.info("onStartFollowing: {}", p0)
+        logger.info("Node is now following a leader: {}", p0?.leaderId)
     }
 
     override fun onStopFollowing(p0: LeaderChangeContext?) {
-        logger.info("onStopFollowing: {}", p0)
+        logger.info("Node is no longer following a leader: {}", p0?.leaderId)
     }
+
+    override fun onError(e: RaftException?) {
+        logger.error(e?.message, e)
+    }
+
 }
